fdea18c4216ad44ee1796e3b5a54e493698c9655
Lucy <luzharck@gmail.com>
2023-04-25 00:55:02 -0500

    firt commit

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+document.querySelector("#Home").removeAttribute("hidden");
+
+const navAnchors = document.querySelectorAll('button#menuButton');
+
+navAnchors.forEach((anchor) => {
+
+  anchor.addEventListener("click", (event) => {
+
+    document.querySelectorAll(".screen").forEach((screen) => {
+      screen.setAttribute("hidden", true)
+    });
+
+
+    // read screen attribute
+    let target = anchor.getAttribute("data-screen");
+    console.log(target);
+    // find target screen
+    let targetScreen = document.querySelector("#" + target);
+
+    // show it
+    targetScreen.removeAttribute("hidden");
+  })
+});
